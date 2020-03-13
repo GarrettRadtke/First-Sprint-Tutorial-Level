c0dde62c3dadad3abf9af1b08767df2badd63bd8
Garrett Radtke <60516972+GarrettRadtke@users.noreply.github.com>
2020-03-13 10:04:34 -0500

    Initial commit

---
+++ Assets/DialogueManage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManage : MonoBehaviour
+{
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+        
+    }
+
+  
+}
---
+++ Assets/scri.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scri : MonoBehaviour
+{
+    string Level = "";
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Application.Quit();
+    }
+}
